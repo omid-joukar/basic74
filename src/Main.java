a5829177ce9d6ac97bbbb2c9ba9f4acad71765c2
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-13 09:53:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by KPS on 7/13/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        int[] num_array = {10, -20, 0, 30, 40, 60, 10};
+        System.out.println((num_array[0] == 10 || num_array[num_array.length-1] == 10));
+    }
+}
